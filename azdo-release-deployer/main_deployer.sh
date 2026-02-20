eddc0764d8ab6b7ea1d61e45b05bb968d414b8a8
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-02-20 21:51:22 +0800

    Fix deployment variable assignments in main_deployer.sh

--- azdo-release-deployer/main_deployer.sh
+++ azdo-release-deployer/main_deployer.sh
@@ -13,3 +13,23 @@ loadvars() {
    NAME_UPPER=$(RELEASE_ENVIRONMENTNAME^^}
 
    
+case "$NAME_UPPER" in
+*DEV*
+echo "Load DEV Bucket"
+echo 'export BUCKET_NAME="wsp-dev-use2-s3-1ambda-deployment-package"* »> $VARS
+if [I SNAME_UPPER == "DEV1 DEPLOYMENT" ]]; then
+echo 'export TFVARS_PATH="/opt/agent/tfvars/dev1_use2_v1.tfvars"* >> $VARS
+echo 'export ENV_IAC="dev1_use2_v1"' » $VARS
+echo 'export ENV_PACKAGE="dev1_v1"' »› $VARS elif [[ $NAME_UPPER == "DEV DEPLOYMENT" ]l; then
+echo 'export TFVARS_PATH="/opt/agent/tfvars/dev1_use2_v1.tfvars"* ›> $VARS
+echo 'export ENV_TAC="dev_use2"' »> SVARS
+echo 'export ENV_PACKAGE="dev_v1"' »> $VARS
+elif [I SNAME_UPPER = "DEV2 DEPLOYMENT" 11; then
+echo 'export TFVARS_PATH="/opt/agent/tfvars/dev2_use2_v1.tfvars"* »> $VARS
+echo 'export ENV_IAC="dev2_use2_v1"' »> $VARS
+echo 'export ENV_PACKAGE="dev2_v1"' »> $VARS
+elif L[ SNAME_UPPER = "DEV3 DEPLOYMENT" 11; then
+echo 'export TFVARS_PATH="/opt/agent/tfvars/dev3_use2_v1.tfvars"* >> $VARS
+echo 'export ENV_TAC="dev3_use2_v1"* »> $VARS
+echo 'export ENV_PACKAGE="dev3_v1"' >> $VARS
+else
